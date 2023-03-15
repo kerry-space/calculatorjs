35f669fd18fb49cd43bb78fbd41c8cc67810541f
davidIvankov <david.ivankov96@gmail.com>
2023-03-15 18:51:19 +0100

    feat: added clear and backspace

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { mathOperation } from "./math.js"
-const calculatorButtons = document.querySelectorAll('input')
+const calculatorButtons = document.querySelectorAll('button')
 , display = document.getElementById('display');
 
 
@@ -12,7 +12,31 @@ let numString = ''
 display.innerText = ''
 
 
-const equalOperator = function() {
+const inputZero = function(eKey) {
+    if (operator === '/' && !numString ) {
+        console.log('error message')
+    } else {
+        display.innerText += eKey;
+            numString += eKey;
+    }
+
+}
+,handleBackspace = function() {
+  display.innerText =  display.innerText.slice(0, -1);
+  numString = numString.slice(0, -1);
+  if (!display.innerText) {
+    return;
+  } 
+
+} 
+,handleDelete = function() {
+    numString = '';
+        operator = '';
+        num1 = undefined;
+        num2 = undefined;
+        display.innerText = '';
+}
+,equalOperator = function() {
      if (!display.innerText) {
             console.log('error Input');
         }
@@ -20,6 +44,8 @@ const equalOperator = function() {
         return console.log('error message')
       } else { 
         num2 = Number(numString);
+        console.log({numString, num1, num2})
+
         numString = '';
         display.innerText = mathOperation(num1, num2, operator);
         operator = ''
@@ -30,7 +56,17 @@ const equalOperator = function() {
         if (eKey !== button.value) return;
         if ((/[-+*\/\=]/).test(eKey)) {
             insertOperator(eKey)
-        }
+        } else if (eKey === 'Delete') {
+      handleDelete();
+    } else if (eKey === 'Backspace') {
+        handleBackspace()
+    }
+    else if (display.innerText === '0') {
+        console.log('error message')
+
+    } else if (eKey === '0'){
+        inputZero(eKey)
+    }
         else if (eKey === button.value) {
             display.innerText += button.value;
             numString += button.value; 
@@ -55,7 +91,7 @@ const equalOperator = function() {
         num1 = 0;
         operator = eKey;
         display.innerText = `0 ${eKey}`;
-    } else {
+    }  else {
         console.log(numString)
         num1 = Number(numString);
         numString = ''
@@ -66,7 +102,17 @@ const equalOperator = function() {
 }
 
 document.addEventListener('keypress', (e) => {
-   InputHandling(e.key);
+    console.log(e.key)
+   InputHandling(e.key,);
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Delete') {
+        handleDelete();
+    } else if (e.key === 'Backspace') {
+        handleBackspace();
+    }
+   
 });
 
 document.addEventListener('click', (e) => {
