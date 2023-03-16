57035b4a0312dadb6798522fe9cf42c6d8a84a7d
davidIvankov <david.ivankov96@gmail.com>
2023-03-16 20:26:15 +0100

    precision

--- math.js
+++ math.js
@@ -1,18 +1,17 @@
 const  mathOperation = function(num1, num2, operator ){
     switch(operator){
         case "+":
-           return (num1 + num2).toFixed(2);
+           return (num1 + num2).toPrecision(2);
         
         case "-":
-            return (num1 - num2).toFixed(2);
+            return (num1 - num2).toPrecision(2);
            
         case "*":
-            return (num1 * num2).toFixed(2);
+            return (num1 * num2).toPrecision(2);
 
         case "/":
-            return (num1 / num2).toFixed(2);
+            return (num1 / num2).toPrecision(2);
         default: 
-            
     }
 }
 
